4afac7dfbaa78545e6d37896459c278f3f7f70b9
dshukertjr <18113850+dshukertjr@users.noreply.github.com>
2021-04-14 14:20:52 +0900

    added db stuff

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,11 @@ import 'dart:io';
 import 'dart:typed_data';
 
 import 'package:flutter/cupertino.dart';
+import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:photo_manager/photo_manager.dart';
 import 'package:flutter_image_compress/flutter_image_compress.dart';
+import 'package:sqflite/sqflite.dart';
 
 void main() {
   runApp(MyApp());
@@ -51,6 +53,7 @@ class HomePage extends StatelessWidget {
 
   Future<void> downloadImagesInit({
     BuildContext context,
+    int categoryId,
     List<AssetEntity> assets,
   }) async {
     final directory = await getApplicationDocumentsDirectory();
@@ -73,6 +76,7 @@ class HomePage extends StatelessWidget {
         (asset) => downloadImages(
           context: context,
           path: path,
+          categoryId: categoryId,
           asset: asset,
         ),
       ));
@@ -84,6 +88,7 @@ class HomePage extends StatelessWidget {
   Future<void> downloadImages({
     BuildContext context,
     String path,
+    int categoryId,
     AssetEntity asset,
   }) async {
     String imageName = await asset.titleAsync;
@@ -101,8 +106,12 @@ class HomePage extends StatelessWidget {
     await thumbFile.writeAsBytes(compressedThumbImage);
 
     // データベースに格納
-    // await insertContentsAndImages(
-    //   context, categoryId, imagePathString, thumbPathString);
+    await insertContentsAndImages(
+      context: context,
+      categoryId: categoryId,
+      imagePathString: imagePathString,
+      thumbPathString: thumbPathString,
+    );
   }
 
   Future<Uint8List> getCompressedThumbImage(image) async {
@@ -127,21 +136,183 @@ class HomePage extends StatelessWidget {
     }
   }
 
-  // insertContentsAndImages(
-  //   BuildContext context, int categoryId, String imagePath, thumbPath) {
-  // var contents = ContentsTable(
-  //     categoryId: categoryId,
-  //     initImagePath: thumbPath,
-  //     multiple: 'false',
-  //     memo: null,
-  //     createdAt: DateTime.now().toString());
-  // DBProvider.db.insertContentData(contents).then((id) {
-  //   var image = ImagesTable(
-  //       contentId: id,
-  //       path: imagePath,
-  //       thumbPath: thumbPath,
-  //       orderIndex: 0,
-  //       createdAt: DateTime.now().toString());
-  // await DBProvider.db.insertImageData(image);
-  // });
+  insertContentsAndImages({
+    BuildContext context,
+    int categoryId,
+    String imagePathString,
+    String thumbPathString,
+  }) async {
+    var contents = ContentsTable(
+      categoryId: categoryId,
+      initImagePath: thumbPathString,
+      multiple: 'false',
+      memo: null,
+      createdAt: DateTime.now().toString(),
+    );
+    final id = await DBProvider.db.insertContentData(contents);
+    final image = ImagesTable(
+        contentId: id,
+        path: imagePathString,
+        thumbPath: thumbPathString,
+        orderIndex: 0,
+        createdAt: DateTime.now().toString());
+    return DBProvider.db.insertImageData(image);
+  }
+}
+
+class DBProvider {
+  DBProvider._();
+
+  static final DBProvider db = DBProvider._();
+
+  Database _databaseData;
+
+  Future<Database> get database async {
+    if (_databaseData != null) {
+      return _databaseData;
+    } else {
+      _databaseData = await initDB();
+      return _databaseData;
+    }
+  }
+
+  initDB() async {
+    Directory documentsDirectory = await getApplicationDocumentsDirectory();
+    final path = join(documentsDirectory.path, 'picmemo.db');
+    Database _db = await openDatabase(path, version: 1,
+        onCreate: (Database db, int version) async {
+      await db.execute('''
+              CREATE TABLE all_categories (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                parent_id INTEGER,
+                name TEXT,
+                tag_color_index INTEGER,
+                is_locked TEXT,
+                created_at TEXT,
+                update_at TEXT
+              )
+            ''');
+      await db.execute('''
+              CREATE TABLE contents (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                category_id INTEGER,
+                init_image_path TEXT,
+                multiple TEXT,
+                memo TEXT,
+                created_at TEXT,
+                update_at TEXT
+              )
+            ''');
+      await db.execute('''
+              CREATE TABLE images (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                content_id INTEGER,
+                path TEXT,
+                thumb_path TEXT,
+                order_index INTEGER,
+                created_at TEXT,
+                update_at TEXT
+              )
+            ''');
+    });
+    return _db;
+  }
+
+  insertContentData(ContentsTable contentsTable) async {
+    final _db = await database;
+    var result = await _db.insert(
+      'contents',
+      contentsTable.insertToMap(),
+      conflictAlgorithm: ConflictAlgorithm.replace,
+    );
+    return result;
+  }
+
+  insertImageData(ImagesTable imagesTable) async {
+    final _db = await database;
+    var result = await _db.insert(
+      'images',
+      imagesTable.insertToMap(),
+      conflictAlgorithm: ConflictAlgorithm.replace,
+    );
+    return result;
+  }
+}
+
+class ContentsTable {
+  final int id;
+  final int categoryId;
+  final String initImagePath;
+  final String multiple;
+  final String memo;
+  final String createdAt;
+  final String updateAt;
+
+  ContentsTable(
+      {this.id,
+      this.categoryId,
+      this.initImagePath,
+      this.multiple,
+      this.memo,
+      this.createdAt,
+      this.updateAt});
+
+  Map<String, dynamic> insertToMap() {
+    return {
+      'category_id': categoryId,
+      'init_image_path': initImagePath,
+      'multiple': multiple,
+      'memo': memo,
+      'created_at': createdAt
+    };
+  }
+
+  Map<String, dynamic> updateToMap() {
+    return {
+      'category_id': categoryId,
+      'init_image_path': initImagePath,
+      'multiple': multiple,
+      'memo': memo,
+      'update_at': updateAt
+    };
+  }
+}
+
+class ImagesTable {
+  final int id;
+  final int contentId;
+  final String path;
+  final String thumbPath;
+  final int orderIndex;
+  final String createdAt;
+  final String updatedAt;
+
+  ImagesTable(
+      {this.id,
+      this.contentId,
+      this.path,
+      this.thumbPath,
+      this.orderIndex,
+      this.createdAt,
+      this.updatedAt});
+
+  Map<String, dynamic> insertToMap() {
+    return {
+      'content_id': contentId,
+      'path': path,
+      'thumb_path': thumbPath,
+      'order_index': orderIndex,
+      'created_at': createdAt
+    };
+  }
+
+  Map<String, dynamic> updateToMap() {
+    return {
+      'content_id': contentId,
+      'path': path,
+      'thumb_path': thumbPath,
+      'order_index': orderIndex,
+      'updated_at': updatedAt
+    };
+  }
 }
